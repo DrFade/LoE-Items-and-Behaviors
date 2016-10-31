c334c94dc88c125909f3c5ec4fd7be00c3de0072
DrFade <mrmineman528@gmail.com>
2016-10-31 18:58:13 -0400

    Update BehaviorDb.TheEther.cs

--- BehaviorDb.TheEther.cs
+++ BehaviorDb.TheEther.cs
@@ -17,33 +17,40 @@ namespace wServer.logic
                 new State(
                     new State(
                         new Wander(0.5),
-                        new Shoot(radius: 15, count: 5, angleOffset: 30 / 3, projectileIndex: 0, coolDown: 700),
-                        new Shoot(radius: 35, projectileIndex: 3, count: 32, coolDown: 60000),
-                        new Shoot(radius: 8.4, count: 6, shootAngle: 60, projectileIndex: 1, coolDown:2000),
-                        new HpLessTransition(0.15, "Boo")
+                        new Shoot(radius: 12, count: 5, angleOffset: 30 / 3, projectileIndex: 0, coolDown: 500),
+                        new Shoot(radius: 30, projectileIndex: 3, count: 32, coolDown: 60000),
+                        new Shoot(radius: 6.4, count: 6, shootAngle: 60, projectileIndex: 1, coolDown:900),
+                        new HpLessTransition(0.15, "BOO")
                     ),
-                    new State("Boo",
+                    new State("BOO",
                         new ConditionalEffect(ConditionEffectIndex.Invulnerable),
                         new Follow(1.5, acquireRange: 14, range:10),
                         new Shoot(radius: 15, count: 5, angleOffset: 30 / 3, projectileIndex: 4, coolDown: 500, predictive: 1),
-                        new Taunt(1.0, true, "NOOO!!"),
-                        new TimedTransition(4344, "ripp")
+                        new HpLessTransition(0.50, "YOU HAVE CROSSED ME FOR THE LAST TIME")
                     ),
-                    new State("ripp",
+                    new State("YOU HAVE CROSSED ME FOR THE LAST TIME,
+                              new Wander(0.5)
+                              new Shoot(radius: 15, count: 8, angleoffset: 30 / 3, projectileIndex: 0, coolDown: 450),
+                              new Shoot(radius: 32, projectileIndex: 3, count: 34, coolDown: 50000),
+                              new Shoot(radius: 7.4, count: 10, shootAngle: 60, projectileIndex: 1, coolDown: 700),
+                              new TimedTransition(4344, "RIPP")
+                              
+                
+                        new State("RIPP",
                         new Shoot(35, projectileIndex: 3, count: 30),
                         new Shoot(35, projectileIndex: 4, count: 30),
                         new Suicide()
                         )
                     ),
                 new MostDamagers(5,
-                    new TierLoot(11, ItemType.Weapon, goodloot),
-                    new TierLoot(12, ItemType.Weapon, greatloot),
-                    new TierLoot(11, ItemType.Armor, normalloot),
-                    new TierLoot(12, ItemType.Armor, goodloot),
-                    new TierLoot(5, ItemType.Ring, goodloot),
-                    new TierLoot(6, ItemType.Ring, greatloot),
-                    new TierLoot(5, ItemType.Ability, goodloot),
-                    new TierLoot(6, ItemType.Ability, greatloot),
+                    new TierLoot(11, ItemType.Weapon, awesomeloot),
+                    new TierLoot(12, ItemType.Weapon, awesomeloot),
+                    new TierLoot(11, ItemType.Armor, awesomeloot),
+                    new TierLoot(12, ItemType.Armor, awesomeloot),
+                    new TierLoot(5, ItemType.Ring, awesomeloot),
+                    new TierLoot(6, ItemType.Ring, awesomeloot),
+                    new TierLoot(5, ItemType.Ability, awesomeloot),
+                    new TierLoot(6, ItemType.Ability, awesomeloot),
                     new ItemLoot("Bone Dagger", awesomeloot)
                     ),
                 new MostDamagers(3,
@@ -61,18 +68,18 @@ namespace wServer.logic
         .Init("Ether Skeleton",
                 new State(
                     new Follow(1.5, acquireRange: 14, range: 10),
-                    new Shoot(15, 3, 5, angleOffset: 30 / 3, projectileIndex: 0, coolDown: 120)
+                    new Shoot(15, 5, 5, angleOffset: 30 / 3, projectileIndex: 0, coolDown: 120)
                 ),
                 new MostDamagers(5,
                     new OnlyOne(
                         new ItemLoot("Bone Dagger", awesomeloot)
                         ),
-                        new TierLoot(5, ItemType.Weapon, poorloot),
-                        new TierLoot(6, ItemType.Weapon, poorloot),
-                        new TierLoot(7, ItemType.Weapon, poorloot),
-                        new TierLoot(9, ItemType.Armor, mediumloot),
-                        new TierLoot(8, ItemType.Armor, mediumloot),
-                        new TierLoot(7, ItemType.Armor, poorloot)
+                        new TierLoot(5, ItemType.Weapon, awesomeloot),
+                        new TierLoot(6, ItemType.Weapon, awesomeloot),
+                        new TierLoot(7, ItemType.Weapon, awesomeloot),
+                        new TierLoot(9, ItemType.Armor, awesomeloot),
+                        new TierLoot(8, ItemType.Armor, awesomeloot),
+                        new TierLoot(7, ItemType.Armor, awesomeloot)
                         )
                 )
             ;
